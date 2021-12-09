0acce4d4241aa8f899d4da26ace40c1b068e1b74
Evgeny <80065479+evgeny224@users.noreply.github.com>
2021-12-09 08:17:23 +0300

    add article and footer

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import "./article.css";
 
 
-function Article() {
+const Article = ( { imgUrl, date, title }) => {
     return (
-        <div>
-            <h1>
-                Article
-            </h1>
+        <div className="gpt__blog-container_article">
+            <div className="gpt__blog-container_article-image">
+                <img src={imgUrl} alt="blog" />
+            </div>
+            <div className="gpt__blog-container_article-content">
+                <div>
+                    <p>{date}</p>
+                    <h3>{title}</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
         </div>
     )
 }
